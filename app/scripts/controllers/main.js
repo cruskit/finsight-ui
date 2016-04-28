00bbbc313a827a3ff6cfa935e93c749c4bfcd1ec
Paul Ruskin <cruskit@gmail.com>
2016-04-28 22:03:12 +1000

    Added retrieval of strategy outcome information.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the finsightUiApp
  */
 angular.module('finsightUiApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+
+    $scope.outcome = '';
+
+    $scope.loadOutcome = function(){
+        $http.get('/finsight/strategy').success(function(data) {
+            $scope.outcome = data;
+            console.log('Loaded Strategy Outcome: ' + angular.toJson(data));
+        }).error(function(){
+            console.log('Unable to load strategy outcome');
+        });
+    };
+
+    $scope.loadOutcome();
+
+  }]);
