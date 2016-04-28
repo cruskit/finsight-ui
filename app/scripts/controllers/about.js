23e34544f598728b9c29f529d202db4d6ab49baa
Paul Ruskin <cruskit@gmail.com>
2016-04-28 21:45:50 +1000

    Initial scaffolding from yeoman.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finsightUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the finsightUiApp
+ */
+angular.module('finsightUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finsightUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the finsightUiApp
+ */
+angular.module('finsightUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
