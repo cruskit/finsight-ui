cbcb13db85a329ef1ab41a3de352e4d0f7d4be1d
Paul Ruskin <cruskit@gmail.com>
2016-04-30 20:46:48 +1000

    Updated results structure returned by service.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,11 +11,11 @@ angular.module('finsightUiApp')
   .controller('MainCtrl', ['$scope', '$http',
     function ($scope, $http) {
 
-    $scope.outcome = '';
+    $scope.strategyResults = '';
 
     $scope.loadOutcome = function(){
         $http.get('/finsight/strategy').success(function(data) {
-            $scope.outcome = data;
+            $scope.strategyResults = data;
             console.log('Loaded Strategy Outcome: ' + angular.toJson(data));
         }).error(function(){
             console.log('Unable to load strategy outcome');
